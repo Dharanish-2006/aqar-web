66c451f77805e5b16e92c9b5a1945e25f9b2b7be
dharanish <dharanishwar.2006@gmail.com>
2026-03-03 13:58:05 +0530

    base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'https://aqar-014n.onrender.com/',
     },
   },
 })
